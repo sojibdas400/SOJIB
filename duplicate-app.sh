aca0ef06df9df35a634f6a5513908634078e8b47
sojib600 <sojibdas400@gmail.com>
2025-03-05 02:35:56 +0600

    bash

--- duplicate-app.sh
+++ duplicate-app.sh
@@ -2,9 +2,10 @@
 NEW_ID="com.yourcompany.app$1"
 NEW_NAME="App Variant $1"
 
-# Android
-sed -i '' "s/applicationId \".*\"/applicationId \"$NEW_ID\"/g" android/app/build.gradle
-sed -i '' "s/<string name=\"app_name\">.*<\/string>/<string name=\"app_name\">$NEW_NAME<\/string>/g" android/app/src/main/res/values/strings.xml
+# Android modifications
+sed -i "s/applicationId \".*\"/applicationId \"$NEW_ID\"/g" android/app/build.gradle
+sed -i "s/<string name=\"app_name\">.*<\/string>/<string name=\"app_name\">$NEW_NAME<\/string>/g" android/app/src/main/res/values/strings.xml
 
-cd android && ./gradlew assembleApp1Release
+# Build command (changed to use debug build)
+cd android && ./gradlew assembleRelease
 cd ..
